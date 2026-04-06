8d4b735a054527b02c3974f613f982f6eda23108
Skyearn <SignUp@viuk.top>
2026-04-07 07:58:05 +0800

    Fix Xcode warnings and restore automatic signing

--- BLEUnlock/AppDelegate.swift
+++ BLEUnlock/AppDelegate.swift
@@ -1,5 +1,5 @@
 import Cocoa
-import Quartz
+import CoreGraphics
 import ServiceManagement
 import UserNotifications
 
--- BLEUnlock/lowlevel.c
+++ BLEUnlock/lowlevel.c
@@ -10,7 +10,24 @@ void wakeDisplay(void)
 
 void sleepDisplay(void)
 {
-    io_registry_entry_t reg = IORegistryEntryFromPath(kIOMainPortDefault, "IOService:/IOResources/IODisplayWrangler");
+    mach_port_t mainPort;
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 120000
+    if (__builtin_available(macOS 12.0, *)) {
+        mainPort = kIOMainPortDefault;
+    } else {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        mainPort = kIOMasterPortDefault;
+#pragma clang diagnostic pop
+    }
+#else
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    mainPort = kIOMasterPortDefault;
+#pragma clang diagnostic pop
+#endif
+
+    io_registry_entry_t reg = IORegistryEntryFromPath(mainPort, "IOService:/IOResources/IODisplayWrangler");
     if (reg) {
         IORegistryEntrySetCFProperty(reg, CFSTR("IORequestIdle"), kCFBooleanTrue);
         IOObjectRelease(reg);
